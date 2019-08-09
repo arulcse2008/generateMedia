d393fa66c8f8d3c2e0044b25fbbc9cd644d03a2a
Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>
2019-08-09 18:22:52 +0530

    parsing parameters from the meta details
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>

--- generateMedia.sh
+++ generateMedia.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 #set -x
+#Samples
+#
+# convert from vob to avi with mpeg4 codec supports
+#ffmpeg -i snatch_1.vob -f avi -c:v mpeg4 -b:v 800k -g 300 -bf 2 -c:a libmp3lame -b:a 128k snatch.avi
+#
+
+# TODO
+# Limitations
+# 10 bit videos are not supported
+# only support yuv encoded videos so far
+
 
 # Media info parameters
 mediaHeader=('General Complete name' 'General File Type' 'General Format' 'General Format profile' 'General Format Info' \
@@ -18,47 +29,8 @@ mediaHeader=('General Complete name' 'General File Type' 'General Format' 'Gener
 	 'Audio Format' 'Audio Format/Info' 'Audio Format profile' 'Audio Codec ID' 'Audio Duration' \
 	 'Audio Bit rate mode' 'Audio Bit rate' 'Audio Channel(s)' 'Audio Channel positions' \
 	 'Audio Sampling rate' 'Audio Frame rate' 'Audio Compression mode' 'Audio Stream size' \
-	 'Audio Writing library' 'Audio Encoding settings' 'Audio Default' 'Audio Forced' \
+	 'Audio Bit depth' 'Audio Writing library' 'Audio Encoding settings' 'Audio Default' 'Audio Forced' \
 	 'Audio Encoded date' 'Audio Tagged date')
-
-#Values are derived from mediaHeader
-FILE_NAME=0
-MEDIA_TYPE=1
-MEDIA_FORMAT=2
-
-#Image related macros
-IMAGE_FORMAT=2
-IMAGE_FORMAT_COMPRES=13
-IMAGE_WIDTH=14
-IMAGE_HEIGHT=15
-IMAGE_BIT_DEPTH=16
-IMAGE_COLOR_SPACE=17
-IMAGE_COMPRES_MODE=19
-
-#Video related macros
-VIDEO_CODEC_ID=28
-VIDEO_DURATION=30
-VIDEO_BITRATE=31
-VIDEO_WIDTH=32
-VIDEO_HEIGHT=33
-VIDEO_ASPECT_RATIO=34
-VIDEO_FRAME_RATE=36
-VIDEO_COLOR_SPACE=37
-VIDEO_BIT_DEPTHS=39
-
-#Audio related macros
-AUDIO_
-AUDIO_
-AUDIO_
-AUDIO_
-AUDIO_
-AUDIO_
-AUDIO_
-AUDIO_
-
-
-declare  mediaData
-declare  mediaCommand
 
 #Check whether the ffmpeg is installed in the system or not?
 which ffmpeg > /dev/null
@@ -74,53 +46,6 @@ usage ()
     echo "Usage: $0 -m <mediaInfo.csv> -i <Input reference media>"
     echo "eg: $0 -m mediaDetails.csv -i ReferenceVideo.mp4"
     exit 1
-}
-
-printVideoParams ()
-{
-}
-
-
-printAudioParams ()
-{
-}
-
-printImageParams ()
-{
-}
-
-#Values are derived from mediaHeader
-FILE_NAME=0
-MEDIA_TYPE=1
-MEDIA_FORMAT=2
-
-generateMedia ()
-{
-	#Collect all the elements from the file and take action accordingly
-	metaMedia=("$@")
-
-	#getting the only filename from the the completename
-	fileName=$(basename "${metaMedia[$FILE_NAME]}")
-
-	#check the mediatype
-	case ${metaMedia[$MEDIA_TYPE]} in
-		"Image")
-			echo "Input media is an image and name $fileName"
-			printImageParams "${metaMedia[@]}"
-			;;
-		"Audio")
-			echo "Input media is an audio and name $fileName"
-			printAudioParams "${metaMedia[@]}"
-			;;
-		"Video")
-			echo "Input media is an video and name $fileName"
-			
-			printVideoparams "${metaMedia[@]}"
-			;;
-		*)
-		echo "Invalid media type and name $fileName"
-		return
-	esac
 }
 
 #check input arguments
@@ -154,6 +79,8 @@ then
 	usage
 fi
 
+inputRefMedia=$4
+
 fileCount=0
 declare mediaHeaderArr
 
@@ -161,10 +88,216 @@ declare mediaHeaderArr
 rm -fr output_old
 
 #backup previous results
-mv output output_old
+mv -f output output_old
 
 #creating directory to store files
-mkdir output
+mkdir -p output
+
+#MACRO Definitions (Values are derived from mediaHeader)
+#General Macros
+MINTOSECOND=60
+FILE_NAME=0
+MEDIA_TYPE=1
+MEDIA_FORMAT=2
+
+#Image related macros
+IMAGE_FORMAT=2
+IMAGE_FORMAT_COMPRES=13
+IMAGE_WIDTH=14
+IMAGE_HEIGHT=15
+IMAGE_BIT_DEPTH=16
+IMAGE_COLOR_SPACE=17
+IMAGE_COMPRES_MODE=19
+
+#Video related macros
+VIDEO_CODEC_ID=28
+VIDEO_FORMAT=29
+VIDEO_DURATION=31
+VIDEO_BITRATE=32
+VIDEO_WIDTH=33
+VIDEO_HEIGHT=34
+VIDEO_ASPECT_RATIO=35
+VIDEO_FRAME_RATE=37
+VIDEO_COLOR_SPACE=38
+VIDEO_CHROMA_SUBSAMPLING=39
+VIDEO_BIT_DEPTHS=40
+
+#Audio related macros
+AUDIO_FORMAT=52
+AUDIO_DURATION=56
+AUDIO_BIT_RATE=59
+AUDIO_CHANNELS=60
+AUDIO_SAMPLING_RATE=61
+AUDIO_BIT_DEPTH=65
+
+#Arrays of meta details, meta headers
+declare mediaData
+declare metaMedia
+declare mediaCommand
+
+#General Parameters
+fileName=""
+
+#Video Parameters
+videoCodecId=""
+videoFormat=""
+videoDuration=""
+videoBitRate=""
+videoWidth=""
+videoHeight=""
+videoAspectRatio=""
+videoFrameRate=""
+videoColorSpace=""
+videoBitDepth=""
+videoChromaSubSampling=""
+
+#Audio Parameters
+audioBitRate=""
+
+#Image Parameters
+
+getVideoParams ()
+{
+	seconds=""
+	minutes=""
+	#-c:v "codec name" should be used
+	videoCodecId=${metaMedia[VIDEO_CODEC_ID]}
+
+	#videoFormat -f "avc" should be used
+	videoFormat=${metaMedia[VIDEO_FORMAT]}
+
+	#-t should be used with seconds
+	if [[ ${metaMedia[VIDEO_DURATION]} =~ "min" ]];
+	then
+		seconds=$(echo ${metaMedia[VIDEO_DURATION]}|tr -d ' '|awk -F "min" '{print $2}'|awk -F "s" '{print $1}')
+		minutes=$(echo ${metaMedia[VIDEO_DURATION]}|tr -d ' '|awk -F "min" '{print $1}')
+		videoDuration=`expr $minutes \* $MINTOSECOND + $seconds`
+	else
+		videoDuration=$(echo ${metaMedia[VIDEO_DURATION]}|tr -d ' '|awk -F "s" '{print $1}')
+	fi
+
+	#increment one second for rounding off
+	videoDuration=$(($videoDuration+1))
+
+	#figure out the bitrate of the video
+	videoBitRate=$(echo ${metaMedia[VIDEO_BITRATE]}|tr -d ' '|awk -F "b/s" '{print $1}')
+
+	#scaling resolutions -vf scale=1920:1080
+	videoWidth=$(echo ${metaMedia[VIDEO_WIDTH]}|awk -F "pixels" '{print $1}' |tr -d ' ')
+	videoHeight=$(echo ${metaMedia[VIDEO_HEIGHT]}|awk -F "pixels" '{print $1}' |tr -d ' ')
+
+	#-aspect should be used "-aspect 16:9" or "-aspect 4:3"
+	videoAspectRatio=${metaMedia[VIDEO_ASPECT_RATIO]}
+
+	#-r should be used -r 24 (for 24fps)
+	if [[ ${metaMedia[VIDEO_FRAME_RATE]} =~ "(" ]];
+	then
+		videoFrameRate=$(echo ${metaMedia[VIDEO_FRAME_RATE]}|tr -d ' '|awk -F "(" '{print $1}')
+	else
+		videoFrameRate=$(echo ${metaMedia[VIDEO_FRAME_RATE]}|tr -d ' '|awk -F "FPS" '{print $1}')
+	fi
+
+
+	#assign only lower case for yuv
+	#TODO need to check for colorspace apart from  yuv
+	videoColorSpace=${metaMedia[VIDEO_COLOR_SPACE],,}
+
+	#convert 4:2:2 to 422
+	if [ ${metaMedia[VIDEO_CHROMA_SUBSAMPLING]} = "0" ]
+	then
+		videoChromaSubSampling="NA"
+	else
+		videoChromaSubSampling=$(echo ${metaMedia[VIDEO_CHROMA_SUBSAMPLING]}|awk -F ":" '{print $1$2$3}')
+	fi
+
+	videoBitDepth=$(echo ${metaMedia[VIDEO_BIT_DEPTHS]}|awk -F "bits" '{print $1}'|tr -d ' ')
+
+	audioBitRate=$(echo ${metaMedia[AUDIO_BIT_RATE]}|awk -F "b/s" '{print $1}'|tr -d ' ')
+}
+
+getAudioParams ()
+{
+	echo "audio bitrate:" $audioBitRate
+}
+
+getImageParams ()
+{
+	echo "I am dummy getImageParams"
+}
+
+printVideoParams ()
+{
+	echo "Video parameters"
+	echo "codect id:" $videoCodecId
+	echo "videoFormat" $videoFormat
+	echo "duration:" $videoDuration
+	echo "bitrate:" $videoBitRate
+	echo "Resolution:" $videoWidth"x"$videoHeight 
+	echo "aspect ratio:"$videoAspectRatio
+	echo "FPS:" $videoFrameRate
+	echo "Bitdepth:" $videoBitDepth
+	# TODO 10 bit videos are not supported
+	echo "Colorspace:" $videoColorSpace
+	echo "chromasubsampling:" $videoChromaSubSampling
+}
+
+printAudioParams ()
+{
+	echo "I am dummy getAudioParams"
+}
+
+printImageParams ()
+{
+	echo "I am dummy getAudioParams"
+}
+
+cmdToGenerateVideo=""
+generateVideo ()
+{
+	if [ $videoChromaSubSampling = "NA" ]
+	then
+		#skip chroma and colorspace configuration, input file config will be taken
+		cmdToGenerateVideo=$(echo "ffmpeg -i" $inputRefMedia "-vf scale="$videoWidth":"$videoHeight "-r" $videoFrameRate "-aspect" $videoAspectRatio "-t" $videoDuration "-b:v" $videoBitRate "-b:a" $audioBitRate  "output/"$fileName)
+	else
+		cmdToGenerateVideo=$(echo "ffmpeg -i" $inputRefMedia "-vf scale="$videoWidth":"$videoHeight "-pix_fmt "$videoColorSpace$videoChromaSubSampling"p" "-r" $videoFrameRate "-aspect" $videoAspectRatio "-t" $videoDuration "-b:v" $videoBitRate "-b:a" $audioBitRate  "output/"$fileName)
+	fi
+
+	echo "executing ffmpeg cmd:" $cmdToGenerateVideo
+	$cmdToGenerateVideo
+}
+
+generateMedia ()
+{
+	#Collect all the elements from the file and take action accordingly
+	metaMedia=("$@")
+
+	#getting the only filename from the the completename
+	fileName=$(basename "${metaMedia[$FILE_NAME]}")
+	
+	#check the mediatype
+	echo "Input media type:  ${metaMedia[$MEDIA_TYPE]} and name $fileName"
+	case ${metaMedia[$MEDIA_TYPE]} in
+		"Image")
+			printImageParams "${metaMedia[@]}"
+			;;
+		"Audio")
+			printAudioParams "${metaMedia[@]}"
+			;;
+		"Video")
+			#Get the Video parameters from each input field
+			getVideoParams "${metaMedia[@]}"
+
+			#Print processed Video parameters
+			printVideoParams
+
+			#Generate the video files based on the processed input
+			generateVideo
+			;;
+		*)
+		echo "Invalid media type and name $fileName"
+		return
+	esac
+}
 
 #Processing mediaInfo file
 while read -r eachMediaRow
@@ -191,7 +324,8 @@ do
 		generateMedia "${mediaHeaderArr[@]}"
 	fi
 
+	#To calculate the number of processed metadata
 	fileCount=$((fileCount+1))
 done < $2
-echo "Generating media files are completed for $fileCount files. Files are generated under $PWD/output directory"
+echo "Generating media files are completed for $((fileCount-1)) files. Files are generated under $PWD/output directory"
 exit 0
